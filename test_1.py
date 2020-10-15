7309852a3603598929beaae7dd39c970ce8bcffa
aFelipeSP <cepfelo@gmail.com>
2020-10-15 11:20:49 -0500

    Create test_1.py

---
+++ test_1.py
@@ -0,0 +1,19 @@
+import main
+
+def test_entero():
+    assert main.entero == 1
+    
+def test_decimal():
+    assert main.decimal == 3.14
+    
+def test_texto():
+    assert 'hola mundo' in main.texto.lower().strip()
+    
+def test_booleano():
+    assert main.booleano == True
+    
+def test_lista():
+    assert main.lista == [main.numero, main.decimal, main.texto, main.booleano]
+    
+def test_dict():
+    assert main.dict == {'entero': main.entero, 'decimal': main.decimal, 'texto': main.texto, 'booleano': main.booleano, 'lista': main.lista}
